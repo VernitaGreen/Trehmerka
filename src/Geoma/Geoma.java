be4b1bfa642b80a87a99f55b8cab8c5c5b55e4c3
Nika <nika@grinina.me>
2019-02-18 10:59:33 +0400

    fix

---
+++ src/Geoma/Geoma.java
@@ -0,0 +1,25 @@
+package Geoma;
+
+import base.Line;
+import base.Plane;
+import base.Point2D;
+
+public class Geoma {
+  public static Point2D projectionPointToPlane(Point2D point, Plane plane) {
+//    TODO
+    return new Point2D(point.x, point.y);
+  }
+
+  public static double heightPointToPlane(Point2D point, Plane plane) {
+    return .0;
+  }
+
+  public static Point2D projectionPointToLine(Point2D point, Line line) {
+//    TODO
+    return new Point2D(point.x, point.y);
+  }
+
+  public static double heightPointToLine(Point2D point, Line line) {
+    return .0;
+  }
+}
---
+++ src/Risovalka/DrawOBJ.java
@@ -0,0 +1,4 @@
+package Risovalka;
+
+public class DrawOBJ {
+}
