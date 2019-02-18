06c9295ae5c7c2b4e18bfa90462f0d9abaf3eafb
Nika <nika@grinina.me>
2019-02-18 11:20:01 +0400

    solve quadratic eq

--- src/Geoma/Geoma.java
+++ src/Geoma/Geoma.java
@@ -3,23 +3,16 @@ package Geoma;
 import base.Line;
 import base.Plane;
 import base.Point2D;
+import base.Vector;
 
 public class Geoma {
-  public static Point2D projectionPointToPlane(Point2D point, Plane plane) {
-//    TODO
-    return new Point2D(point.x, point.y);
-  }
 
-  public static double heightPointToPlane(Point2D point, Plane plane) {
-    return .0;
-  }
-
-  public static Point2D projectionPointToLine(Point2D point, Line line) {
-//    TODO
-    return new Point2D(point.x, point.y);
-  }
-
-  public static double heightPointToLine(Point2D point, Line line) {
-    return .0;
+  
+  double[] solveQuadraticEquation(double a, double b, double c) {
+    double[] ans = new double[2];
+    double d = b * b - 4 * a * c;
+    ans[0] = -b + Math.sqrt(d) / 2 * a;
+    ans[1] = -b + Math.sqrt(d) / 2 * a;
+    return ans;
   }
 }
--- src/base/Line.java
+++ src/base/Line.java
@@ -2,31 +2,4 @@ package base;
 
 public class Line {
 //  TODO
-  Vector a;
-  Vector b;
-
-  public Line(Vector a, Vector b) {
-    this.a = a;
-    this.b = b;
-  }
-
-  public boolean isPointBelonging(Vector point) {
-    double dx1 = b.x - a.x;
-    double dy1 = b.y - a.y;
-    double dz1 = b.z - a.z;
-
-    double dx2 = point.x - a.x;
-    double dy2 = point.y - a.y;
-    double dz2 = point.z - a.z;
-
-    double eps = 1e-5;
-
-    double c1 = dx1 * dy2 * dz2;
-    double c2 = dx2 * dy1 * dz2;
-    double c3 = dx2 * dy2 * dz1;
-
-    return Math.abs(c1 - c2) < eps && Math.abs(c2 - c3) < eps;
-  }
-
-
 }
