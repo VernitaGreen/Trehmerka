c337233d32872f354a099032efda3fab682ed8ca
Nika <nika@grinina.me>
2019-03-06 20:48:30 +0400

    test

--- src/Geoma/Geoma.java
+++ src/Geoma/Geoma.java
@@ -7,8 +7,7 @@ import base.Vector;
 
 public class Geoma {
 
-  
-  double[] solveQuadraticEquation(double a, double b, double c) {
+  public  static double[] solveQuadraticEquation(double a, double b, double c) {
     double[] ans = new double[2];
     double d = b * b - 4 * a * c;
     ans[0] = -b + Math.sqrt(d) / 2 * a;
--- test/base/GTest.java
+++ test/base/GTest.java
@@ -24,5 +24,14 @@ public class GTest {
         new Vector(0, 1, 0));
 
     assertNull(G.intersect(plane, new Ray(new Vector(0, 0, 1), new Vector(0, 1, 1))));
+
+
+//    тесты ники
+    Plane plane1 = new Plane(new Vector(2, 0, 5), new Vector(0, 0, 1));
+
+    assertEquals(
+        G.intersect(plane1, new Ray(new Vector(4, 0, 2), new Vector(2, 0, 3))),
+        new Vector(6, 0, 5)
+    );
   }
 }
